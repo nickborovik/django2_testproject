174311f54c2e0a18767cc7d51729fbbc1670ac4d
Nikita Borovik <nick.borovik@gmail.com>
2020-05-21 13:07:55 +0300

    lesson 6 (save)

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,34 @@
 from django.shortcuts import render
-from .models import Post
+from django.views.generic import View
+from .models import Post, Tag
 
 # Create your views here.
 def posts_list(request):
     posts = Post.objects.all()
     return render(request, 'blog/index.html', context={'posts': posts})
 
-def post_detail(request, slug):
-    post = Post.objects.get(slug__iexact=slug)
-    return render(request, 'blog/post_detail.html', context={'post': post})
+
+# def post_detail(request, slug):
+#     post = Post.objects.get(slug__iexact=slug)
+#     return render(request, 'blog/post_detail.html', context={'post': post})
+
+class PostDetail(View):
+    def get(self, request, slug):
+        post = Post.objects.get(slug__iexact=slug)
+        return render(request, 'blog/post_detail.html', context={'post': post})
+
+
+# def tag_detail(request, slug):
+#     tag = Tag.objects.get(slug__iexact=slug)
+#     return render(request, 'blog/tag_detail.html', context={'tag': tag})
+
+
+class TagDetail(View):
+    def get(self, request, slug):
+        tag = Tag.objects.get(slug__iexact=slug)
+        return render(request, 'blog/tag_detail.html', context={'tag': tag})
+
+
+def tags_list(request):
+    tags = Tag.objects.all()
+    return render(request, 'blog/tags_list.html', context={'tags': tags})
