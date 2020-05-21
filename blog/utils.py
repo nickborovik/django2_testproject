20f94d66c562a0b0012d7970215aed4c682f69c2
Nikita Borovik <nick.borovik@gmail.com>
2020-05-21 16:36:18 +0300

    lesson 8 (save)

---
+++ blog/utils.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from django.shortcuts import redirect
+from django.shortcuts import get_object_or_404
+
+from .models import *
+
+
+class ObjectDetailMixin:
+    model = None
+    template = None
+
+    def get(self, request, slug):
+        obj = get_object_or_404(self.model, slug__iexact=slug)
+        return render(request, self.template, context={self.model.__name__.lower(): obj})
+
+class ObjectCreateMixin:
+    model_form = None
+    template = None
+
+    def get(self, request):
+        form = self.model_form()
+        return render(request, self.template, context={'form': form})
+
+    def post(self, request):
+        bound_form = self.model_form(request.POST)
+
+        if bound_form.is_valid():
+            new_obj = bound_form.save()
+            return redirect(new_obj)
+        return render(request, 'blog/tag_create.html', context={'form': bound_form})
--- blog/views.py
+++ blog/views.py
@@ -1,32 +1,57 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from django.shortcuts import get_object_or_404
 from django.views.generic import View
-from .models import Post, Tag
 
-# Create your views here.
+from .models import Post, Tag
+from .utils import *
+from .forms import TagForm, PostForm
+
+
 def posts_list(request):
     posts = Post.objects.all()
     return render(request, 'blog/index.html', context={'posts': posts})
 
 
-# def post_detail(request, slug):
-#     post = Post.objects.get(slug__iexact=slug)
-#     return render(request, 'blog/post_detail.html', context={'post': post})
-
-class PostDetail(View):
-    def get(self, request, slug):
-        post = Post.objects.get(slug__iexact=slug)
-        return render(request, 'blog/post_detail.html', context={'post': post})
+class PostDetail(ObjectDetailMixin, View):
+    model = Post
+    template = 'blog/post_detail.html'
 
 
-# def tag_detail(request, slug):
-#     tag = Tag.objects.get(slug__iexact=slug)
-#     return render(request, 'blog/tag_detail.html', context={'tag': tag})
+class PostCreate(ObjectCreateMixin, View):
+    model_form = PostForm
+    template = 'blog/post_create_form.html'
+    # def get(self, request):
+    #     form = PostForm()
+    #     return render(request, 'blog/post_create_form.html', context={'form': form})
+    #
+    # def post(self, request):
+    #     bound_form = PostForm(request.POST)
+    #     if bound_form.is_valid():
+    #         new_post = bound_form.save()
+    #         return redirect(new_post)
+    #     return render(request, 'blog/post_create_form.html', context={'form': bound_form})
 
 
-class TagDetail(View):
-    def get(self, request, slug):
-        tag = Tag.objects.get(slug__iexact=slug)
-        return render(request, 'blog/tag_detail.html', context={'tag': tag})
+class TagDetail(ObjectDetailMixin, View):
+    model = Tag
+    template = 'blog/tag_detail.html'
+
+
+class TagCreate(ObjectCreateMixin, View):
+    model_form = TagForm
+    template = 'blog/tag_create.html'
+    # def get(self, request):
+    #     form = TagForm()
+    #     return render(request, 'blog/tag_create.html', context={'form': form})
+    #
+    # def post(self, request):
+    #     bound_form = TagForm(request.POST)
+    #
+    #     if bound_form.is_valid():
+    #         new_tag = bound_form.save()
+    #         return redirect(new_tag)
+    #     return render(request, 'blog/tag_create.html', context={'form': bound_form})
 
 
 def tags_list(request):
